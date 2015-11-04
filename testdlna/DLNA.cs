807dd7ac12aba8ea2bf214826aa8be130eef30f9
WendyH <Wendy.Hane@gmail.com>
2015-11-04 18:54:34 +0600

    just created

---
+++ testdlna/DLNA.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace testdlna {
+	internal sealed class DLNA {
+		public HttpWebRequest Request;
+		private string deviceAddr = "";
+		private string requestPayload = "";
+
+		private string soapService = "";
+		private string soapCommand = "";
+
+		public string responseText = "";
+		public string responseBody = "";
+		public string UserAgent    = "";
+
+		public DLNA(string deviceIP, string devicePort) {
+			deviceAddr = "http://" + deviceIP + ":" + devicePort;
+		}
+
+		private void SetSOAPAction(string service, string cmd) {
+			soapService = service;
+			soapCommand = cmd;
+			Request = (HttpWebRequest)WebRequest.Create(deviceAddr + "/upnp/control/" + service);
+			Request.KeepAlive = false;
+			//Request.Connection  = "Close";
+			Request.Method      = "POST";
+			Request.ContentType = "text/xml; charset=\"utf-8\"";
+			Request.Credentials = CredentialCache.DefaultCredentials;
+			Request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip");
+			Request.Headers.Set("SOAPACTION", "\"urn:schemas-upnp-org:service:"+ soapService + ":1#" + soapCommand + "\"");
+			Request.Expect      = "";
+			if (UserAgent.Length > 0)
+				Request.UserAgent = UserAgent;
+		}
+
+		public void SetFile(string fileURI, string name, string time, string date) {
+			SetSOAPAction("AVTransport", "SetAVTransportURI");
+			string didlMeta = GetDIDLVideoMetadata(fileURI, name, time, date);
+			string xml = "<CurrentURI>" + fileURI + "</CurrentURI>\n        <CurrentURIMetaData>"+ WebUtility.HtmlEncode(didlMeta) + "</CurrentURIMetaData>";
+			SetPayload(soapCommand, xml);
+			Send();
+        }
+
+		public void Play(int speed = 1) {
+			SetSOAPAction("AVTransport", "Play");
+			SetPayload(soapCommand, "<Speed>"+speed+"</Speed>");
+			Send();
+		}
+
+		private void SetPayload(string cmd, string xmlBody = "", int InstanceID = 0) {
+			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+						 "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n" +
+						 "   <s:Body>\n"    +
+						 "      <u:{1} xmlns:u=\"urn:schemas-upnp-org:service:{0}:1\">\n" +
+						 "        {2}\n"    +
+						 "      </u:{1}>\n" +
+						 "   </s:Body>\n"   +
+						 "</s:Envelope>";
+			if (xmlBody.Length > 0)
+				xmlBody = "<InstanceID>" + InstanceID + "</InstanceID>\n        " + xmlBody;
+			requestPayload = string.Format(xml, soapService, cmd, xmlBody);
+		}
+
+
+		private string GetMIMEfromExt(string extension) {
+			string mime;
+			return VideoMIMEmappings.TryGetValue(extension, out mime) ? mime : "application/octet-stream";
+		}
+
+		private string GetDIDLVideoMetadata(string fileUri, string title, string time, string date) {
+			string mime = GetMIMEfromExt(Path.GetExtension(fileUri));
+            return
+				"<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\">" +
+				"<item id=\"advert\" parentID=\"18\" restricted=\"1\">" +
+				"<upnp:storageMedium>UNKNOWN</upnp:storageMedium>" +
+				"<upnp:writeStatus>UNKNOWN</upnp:writeStatus>" +
+				"<dc:title>"+ title + "</dc:title>" +
+				"<upnp:class>object.item.videoItem</upnp:class>" +
+				"<dc:date>"+ date + "</dc:date>" +
+				"<res protocolInfo=\"http-get:*:"+mime+":*\" duration=\""+time+"\" size=\"0\">" + fileUri + "</res></item></DIDL-Lite>";
+		}
+
+		private void Send() {
+			UTF8Encoding encoding = new UTF8Encoding();
+			responseText = "";
+			responseBody = "";
+
+			try {
+				//Write the payload to the request body.
+				using (Stream requestStream = Request.GetRequestStream()) {
+					requestStream.Write(encoding.GetBytes(requestPayload), 0, encoding.GetByteCount(requestPayload));
+				}
+
+				HttpWebResponse response = Request.GetResponse() as HttpWebResponse;
+                using (Stream rspStm = response.GetResponseStream()) {
+					using (StreamReader reader = new StreamReader(rspStm)) {
+						responseText += "Response Description: " + response.StatusDescription;
+						responseText += "Response Status Code: " + response.StatusCode;
+						responseText += "\r\n\r\n";
+						responseBody = reader.ReadToEnd();
+					}
+				}
+				responseText = "Success: " + response.StatusCode.ToString();
+
+			} catch (Exception ex) {
+				responseText += ex.ToString() + "\r\n";
+				WebException wex = ex as WebException;
+				if (wex != null) {
+					using (var sr = new StreamReader(wex.Response.GetResponseStream())) {
+						responseText = sr.ReadToEnd().Trim();
+						sr.Close();
+					}
+				}
+            }
+        }
+
+		private static IDictionary<string, string> VideoMIMEmappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+
+			#region Big freaking list of mime types
+			{".3g2" , "video/3gpp2"},
+			{".3gp" , "video/3gpp"},
+			{".3gp2", "video/3gpp2"},
+			{".3gpp", "video/3gpp"},
+			{".asf" , "video/x-ms-asf"},
+			{".asr" , "video/x-ms-asf"},
+			{".asx" , "video/x-ms-asf"},
+			{".avi" , "video/x-msvideo"},
+			{".dif" , "video/x-dv"},
+			{".dv"  , "video/x-dv"},
+			{".flv" , "video/x-flv"},
+			{".IVF" , "video/x-ivf"},
+			{".lsf" , "video/x-la-asf"},
+			{".lsx" , "video/x-la-asf"},
+			{".m1v" , "video/mpeg"},
+			{".m2t" , "video/vnd.dlna.mpeg-tts"},
+			{".m2ts", "video/vnd.dlna.mpeg-tts"},
+			{".m2v" , "video/mpeg"},
+			{".m4v" , "video/x-m4v"},
+			{".mod" , "video/mpeg"},
+			{".mov" , "video/quicktime"},
+			{".movie","video/x-sgi-movie"},
+			{".mp2" , "video/mpeg"},
+			{".mp2v", "video/mpeg"},
+			{".mp4" , "video/mp4"},
+			{".mp4v", "video/mp4"},
+			{".mpa" , "video/mpeg"},
+			{".mpe" , "video/mpeg"},
+			{".mpeg", "video/mpeg"},
+			{".mpg" , "video/mpeg"},
+			{".mpv2", "video/mpeg"},
+			{".mqv" , "video/quicktime"},
+			{".mts" , "video/vnd.dlna.mpeg-tts"},
+			{".nsc" , "video/x-ms-asf"},
+			{".qt"  , "video/quicktime"},
+			{".ts"  , "video/vnd.dlna.mpeg-tts"},
+			{".tts" , "video/vnd.dlna.mpeg-tts"},
+			{".vbk" , "video/mpeg"},
+			{".wm"  , "video/x-ms-wm"},
+			{".wmp" , "video/x-ms-wmp"},
+			{".wmv" , "video/x-ms-wmv"},
+			{".wmx" , "video/x-ms-wmx"},
+			{".wvx" , "video/x-ms-wvx"},
+			#endregion
+
+        };
+
+	}
+}
---
+++ testdlna/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace testdlna {
+	class Program {
+		static void Main(string[] args) {
+
+			string userAgent  = "SEC_HHP_Flipps/1.0";
+			string deviceIP   = "192.168.2.3";
+			string devicePort = "52235";
+			string fileURI    = "http://5.9.84.176:8080/app/2LES5e/sd0lGeslRbp8GCXTHsdpEE3jfNaogpnMB68c8bzqbIWRwR3xz8TPVxIx76W+7ijJPgNdKSL0fadYXoPyIdgXco9FWmwFnqVm703hEhet23JILKNo/A1xDVyAAgzwZnyBXqmqwRpbnG3gKK1+hU+ix5j9L5/yvOlSREqLIZ9baL3ozyliyKif0IL53g8wUKmr2RVJx+mimjtXgVttg/oa3myKT4nW4OvvL2n8vKJQxZvcQf3U57o722gGswA8XkcgEKOVjKS0zOC6eAG/+68YWeXegcgtwgeVkcNKMV6B9iO60x+A1lldRH+PwNLBCdahHBKyeJM0ZEIkLsUg3F9eGwYW9EN44GpgV6Wmb0+cn1wnljVtNhPpxDWQQMZ2+H2vpTBD/uChg/Vea8GpRkO+IA4EdLruyYgSAS6LS4q0R2ZrzKxVOszbtHucM.mp4";
+
+			if (args.Length > 0) deviceIP   = args[0];
+			if (args.Length > 1) devicePort = args[1];
+			if (args.Length > 2) userAgent  = args[2];
+			if (args.Length > 3) fileURI    = args[3];
+
+			Console.WriteLine("Устройство: " + deviceIP + ":" + devicePort);
+			Console.WriteLine("Файл: " + fileURI);
+			Console.WriteLine("");
+
+			DLNA dlnaDevice = new DLNA(deviceIP, devicePort);
+
+			dlnaDevice.UserAgent = userAgent;
+
+			Console.WriteLine("Установка адреса файла...");
+
+			dlnaDevice.SetFile(fileURI, "Advertisement", "00:00:30.000", "2015-11-04T11:54:23");
+
+			Console.WriteLine("Ответ от устройства: \n" + dlnaDevice.responseText);
+
+			Console.WriteLine("Play...");
+
+			dlnaDevice.Play();
+
+			Console.WriteLine("Ответ от устройства: \n" + dlnaDevice.responseText);
+		}
+	}
+}
