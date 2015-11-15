073f8c8375f383046d7294c928f34d5ea77e2c89
WendyH <Wendy.Hane@gmail.com>
2015-11-16 03:18:27 +0600

    detect devices in net

--- testdlna/DLNA.cs
+++ testdlna/DLNA.cs
@@ -5,177 +5,250 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace testdlna {
-	internal sealed class DLNA {
-		public HttpWebRequest Request;
-		private string deviceAddr = "";
-		private string requestPayload = "";
+	class DLNA {
+		internal static string LogFile    = Application.ExecutablePath + ".log"; // Файл лога, куда будут записаны ответы и сообщения, если указан ключ -d
+		internal static int    MaxLogSize = 1024 * 1024 * 2; // Максимальный размер файла лога (2 МБ)
 
-		private string soapService = "";
-		private string soapCommand = "";
+		public UPnPDeviceList  Devices        = new UPnPDeviceList();
+		public UPnPDevice      SelectedDevice = new UPnPDevice("");
 
-		public string responseText = "";
-		public string responseBody = "";
+		private HttpWebRequest Request        = null;
+
+		private string RequestPayload = "";
+		private string SoapCommand    = "";
+		private string SoapService    = "";
+		private string NameSpace      = "";
+
+        public string ResponseText = "";
+		public string ResponseBody = "";
 		public string UserAgent    = "";
 
-		public DLNA(string deviceIP, string devicePort) {
-			deviceAddr = "http://" + deviceIP + ":" + devicePort;
+		public bool Debug = false;
+
+		public DLNA() {
 		}
 
-		private void SetSOAPAction(string service, string cmd, int InstanceID) {
-			soapService = service;
-			soapCommand = cmd;
-			string url  = deviceAddr + "/upnp/control/" + service;
-            Request = (HttpWebRequest)WebRequest.Create(url);
-			Request.KeepAlive = false;
-			//Request.Connection  = "Close";
-			Request.Method      = "POST";
+		private void CreateRequest(string url, string method="GET") {
+			Request = (HttpWebRequest)WebRequest.Create(url);
+			Request.Method      = method;
 			Request.ContentType = "text/xml; charset=\"utf-8\"";
 			Request.Credentials = CredentialCache.DefaultCredentials;
-			Request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip");
-			Request.Headers.Set("SOAPACTION", "\"urn:schemas-upnp-org:service:"+ soapService + ":1#" + soapCommand + "\"");
 			Request.Expect      = "";
-			if (UserAgent.Length > 0)
-				Request.UserAgent = UserAgent;
-			Console.WriteLine("URL: "+ url);
+			Request.UserAgent   = UserAgent;
+			Request.Headers.Set(HttpRequestHeader.AcceptEncoding, "gzip");
+        }
+
+		private void SetSOAPAction(string serviceName, string cmd) {
+			SoapService = serviceName;
+			SoapCommand = cmd;
+			try {
+				UPnPService service = SelectedDevice.GetService(serviceName);
+				NameSpace = service.ServiceType;
+				CreateRequest(service.ControlUrl, "POST");
+				Request.Headers.Set("SOAPACTION", "\""+ service.ServiceType + "#" + cmd + "\"");
+			} catch {
+				SelectedDevice.Ready = false;
+			}
 		}
 
 		public void SetFile(string fileURI, string name, string time, string date) {
-			SetSOAPAction("AVTransport", "SetAVTransportURI", 0);
+			SetSOAPAction("AVTransport", "SetAVTransportURI");
 			string didlMeta = GetDIDLVideoMetadata(fileURI, name, time, date);
-			string xml = "<CurrentURI>" + fileURI + "</CurrentURI>\n        <CurrentURIMetaData>"+ WebUtility.HtmlEncode(didlMeta) + "</CurrentURIMetaData>";
-			SetPayload(soapCommand, xml);
+			string metaData = "<CurrentURIMetaData>"+ WebUtility.HtmlEncode(didlMeta) + "</CurrentURIMetaData>";
+			SetPayload("CurrentURI", fileURI, metaData);
 			Send();
-        }
+		}
 
 		public void Play(int speed = 1) {
-			SetSOAPAction("AVTransport", "Play", 0);
-			SetPayload(soapCommand, "<Speed>" + speed + "</Speed>");
+			SetSOAPAction("AVTransport", "Play");
+			SetPayload("Speed", speed.ToString());
 			Send();
 		}
 
-		public void Stop(int speed = 1) {
-			SetSOAPAction("AVTransport", "Stop", 0);
-			SetPayload(soapCommand);
+		public void Stop() {
+			SetSOAPAction("AVTransport", "Stop");
+			SetPayload();
 			Send();
-		}
-
-		private void SetPayload(string cmd, string xmlBody = "", int InstanceID = 0) {
-			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-						 "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n" +
-						 "   <s:Body>\n"    +
-						 "      <u:{1} xmlns:u=\"urn:schemas-upnp-org:service:{0}:1\">\n" +
-						 "        {2}\n"    +
-						 "      </u:{1}>\n" +
-						 "   </s:Body>\n"   +
-						 "</s:Envelope>";
-			if (xmlBody.Length > 0)
-				xmlBody = "<InstanceID>" + InstanceID + "</InstanceID>\n        " + xmlBody;
-			requestPayload = string.Format(xml, soapService, cmd, xmlBody);
-		}
-
-
-		private string GetMIMEfromExt(string extension) {
-			string mime;
-			return VideoMIMEmappings.TryGetValue(extension, out mime) ? mime : "application/octet-stream";
-		}
-
-		private string GetDIDLVideoMetadata(string fileUri, string title, string time, string date) {
-			string mime = GetMIMEfromExt(Path.GetExtension(fileUri));
-            return
-				"<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\">" +
-				"<item id=\"advert\" parentID=\"18\" restricted=\"1\">" +
-				"<upnp:storageMedium>UNKNOWN</upnp:storageMedium>" +
-				"<upnp:writeStatus>UNKNOWN</upnp:writeStatus>" +
-				"<dc:title>"+ title + "</dc:title>" +
-				"<upnp:class>object.item.videoItem</upnp:class>" +
-				"<dc:date>"+ date + "</dc:date>" +
-				"<res protocolInfo=\"http-get:*:"+mime+":*\" duration=\""+time+"\" size=\"0\">" + fileUri + "</res></item></DIDL-Lite>";
 		}
 
 		private void Send() {
-			UTF8Encoding encoding = new UTF8Encoding();
-			responseText = "";
-			responseBody = "";
+			if (!SelectedDevice.Ready) return;
+			//Write the payload to the request body.
+			if (RequestPayload.Length > 0) {
+				using (Stream requestStream = Request.GetRequestStream()) {
+					requestStream.Write(Encoding.UTF8.GetBytes(RequestPayload), 0, Encoding.UTF8.GetByteCount(RequestPayload));
+				}
+			}
+			GetResponse();
+		}
+
+		private string GetResponse(string url = "") {
+			ResponseText = "";
+			ResponseBody = "";
+			WebResponse  response      = null;
+			StreamReader readStream    = null;
+			Stream       receiveStream = null;
+			try {
+				if (url.Length > 0) CreateRequest(url);
+				response      = Request.GetResponse();
+				receiveStream = response.GetResponseStream();
+				readStream    = new StreamReader(receiveStream, Encoding.UTF8);
+				ResponseBody  = readStream.ReadToEnd();
+
+			} catch (WebException e) {
+				Console.WriteLine("Ошибка: " + e.Message + " Статус: " + e.Status.ToString());
+				if (e.Response != null) {
+					ResponseText = new StreamReader(e.Response.GetResponseStream()).ReadToEnd();
+				}
+
+			} catch (Exception e) {
+				Console.WriteLine(e.ToString());
+
+			} finally {
+				if (readStream    != null) readStream   .Close();
+				if (receiveStream != null) receiveStream.Close();
+				if (response      != null) response     .Close();
+			}
+			if (Debug) {
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine("Current SOAP action: " + SoapService + "#" + SoapCommand);
+				sb.AppendLine("Request url: " + Request.RequestUri.AbsoluteUri);
+				sb.AppendLine("Request payload: " + RequestPayload);
+				sb.AppendLine("ResponseText: " + ResponseText);
+				sb.AppendLine("ResponseBody: " + ResponseBody);
+				LogMe(sb.ToString());
+			}
+			return ResponseBody;
+		}
+
+		public void SearchRenderers(bool output2console = false) {
+			IPEndPoint localEndPoint     = new IPEndPoint(IPAddress.Any, 0);
+			IPEndPoint multicastEndPoint = new IPEndPoint(IPAddress.Parse("239.255.255.250"), 1900);
+
+			Socket udpSocket = null;
+			StringBuilder sb = new StringBuilder();
 
 			try {
-				//Write the payload to the request body.
-				using (Stream requestStream = Request.GetRequestStream()) {
-					requestStream.Write(encoding.GetBytes(requestPayload), 0, encoding.GetByteCount(requestPayload));
-				}
+				udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-				HttpWebResponse response = Request.GetResponse() as HttpWebResponse;
-                using (Stream rspStm = response.GetResponseStream()) {
-					using (StreamReader reader = new StreamReader(rspStm)) {
-						responseText += "Response Description: " + response.StatusDescription;
-						responseText += "Response Status Code: " + response.StatusCode;
-						responseText += "\r\n\r\n";
-						responseBody = reader.ReadToEnd();
-					}
-				}
-				responseText = "Success: " + response.StatusCode.ToString();
+				udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+				udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastEndPoint.Address, IPAddress.Any));
+				udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+				udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, true);
 
-			} catch (Exception ex) {
-				responseText += ex.ToString() + "\r\n";
-				WebException wex = ex as WebException;
-				if (wex != null) {
-					using (var sr = new StreamReader(wex.Response.GetResponseStream())) {
-						responseBody = sr.ReadToEnd().Trim();
-						sr.Close();
-					}
+				udpSocket.Bind(localEndPoint);
+
+				string SearchString = "M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nMAN:\"ssdp:discover\"\r\nST:upnp:rootdevice\r\nMX:3\r\nUSER-AGENT: unix/5.1 UPnP/1.0 testdlna/1.0\r\n\r\n";
+
+				udpSocket.SendTo(Encoding.UTF8.GetBytes(SearchString), SocketFlags.None, multicastEndPoint);
+
+				if (output2console) Console.WriteLine("M-Search sent...\r\n");
+				System.Threading.Thread.Sleep(3000);
+
+				byte[] ReceiveBuffer = new byte[64000];
+				int ReceivedBytes = 0;
+
+				while (udpSocket.Available > 0) {
+					ReceivedBytes = udpSocket.Receive(ReceiveBuffer, SocketFlags.None);
+					if (ReceivedBytes > 0)
+						sb.Append(Encoding.UTF8.GetString(ReceiveBuffer, 0, ReceivedBytes));
 				}
+			} finally {
+				if (udpSocket != null)
+					udpSocket.Close();
             }
+			string answer = sb.ToString();
+
+			if (output2console) Console.WriteLine(answer);
+
+			MatchCollection mc = Regex.Matches(answer, @"HTTP/.*?\r\n\r\n", RegexOptions.Singleline);
+			foreach(Match m in mc) {
+				string usn = GetAnswerValue(m.Value, "USN");
+				if (!Devices.Exists(usn)) {
+					UPnPDevice device = new UPnPDevice(usn);
+					device.Type     = GetAnswerValue(m.Value, "ST");
+					device.Location = GetAnswerValue(m.Value, "LOCATION");
+					device.Server   = GetAnswerValue(m.Value, "SERVER");
+					device.HeadersInfo = m.Value;
+					device.DeviceAddr  = Regex.Match(device.Location, @"^.*?//[^/]+").Value;
+
+                    if (device.Location.Length > 0) {
+						string info = GetResponse(device.Location);
+						device.Name         = GetXmlValue(info, "friendlyName");
+						device.Manufacturer = GetXmlValue(info, "manufacturer");
+						device.ModelName    = GetXmlValue(info, "modelName"   );
+						device.UDN          = GetXmlValue(info, "UDN"         );
+                    	device.Type         = GetXmlValue(info, "deviceType"  );
+						MatchCollection mcServices = Regex.Matches(info, @"<service>.*?</service>", RegexOptions.Singleline);
+						foreach (Match s in mcServices) {
+							UPnPService service = new UPnPService();
+							service.ServiceType = GetXmlValue(s.Value, "serviceType");
+							service.ServiceID   = GetXmlValue(s.Value, "serviceId"  );
+							service.SCPDURL     = device.DeviceAddr + GetXmlValue(s.Value, "SCPDURL"    );
+							service.ControlUrl  = device.DeviceAddr + GetXmlValue(s.Value, "controlURL" );
+							service.EventSubURL = device.DeviceAddr + GetXmlValue(s.Value, "eventSubURL");
+							device.Services.Add(service);
+							if (service.ServiceType.IndexOf("AVTransport")>0) device.Ready = true;
+                        }
+					}
+
+					Devices.Add(device);
+				}
+			}
+		}
+
+		public bool SelectDevice(string key) {
+			SelectedDevice = Devices.Select(key);
+			return (SelectedDevice!=null);
+		}
+
+		private string GetAnswerValue(string answer, string name) {
+			return Regex.Match(answer, name+":(.*?)[\r\n]").Groups[1].Value.Trim();
         }
 
-		public void SearchRenderers() {
-			IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Any, 61500);
-			IPEndPoint MulticastEndPoint = new IPEndPoint(IPAddress.Parse("239.255.255.250"), 1900);
+		private string GetXmlValue(string xml, string name) {
+			return Regex.Match(xml, "<"+name+">(.*?)</").Groups[1].Value;
+		}
 
-			Socket UdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		private void SetPayload(string parameterName = "", string parameterValue = "", string additionalData = "") {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+			sb.AppendLine("<s:Envelope s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">");
+			sb.AppendLine("   <s:Body>");
+			sb.AppendLine("      <u:" + SoapCommand + " xmlns:u=\"" + NameSpace + "\">");
+			if (SoapService=="AVTransport") sb.AppendLine  ("         <InstanceID>0</InstanceID>");
+			if (parameterName .Length > 0 ) sb.AppendFormat("         <" + parameterName + ">{0}</" + parameterName + ">\r\n", parameterValue);
+			if (additionalData.Length > 0 ) sb.AppendLine  ("         "  + additionalData);
+			sb.AppendLine("      </u:" + SoapCommand + ">");
+			sb.AppendLine("   </s:Body>");
+			sb.AppendLine("</s:Envelope>");
+			RequestPayload = sb.ToString();
+		}
 
-			UdpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-			UdpSocket.Bind(LocalEndPoint);
-			UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(MulticastEndPoint.Address, IPAddress.Any));
-			UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
-			UdpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastLoopback, true);
+		private string GetDIDLVideoMetadata(string fileUri, string title, string time, string date, int size = 0, string itemId = "advert", string parentId = "0", int restricted = 1) {
+			string mime = GetMIMEfromExt(Path.GetExtension(fileUri));
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\" xmlns:dlna=\"urn:schemas-dlna-org:metadata-1-0/\">");
+			sb.AppendLine("<item id=\"" + itemId + "\" parentID=\"" + parentId + "\" restricted=\"" + restricted + "\">");
+			sb.AppendLine("<upnp:storageMedium>UNKNOWN</upnp:storageMedium>");
+			sb.AppendLine("<upnp:writeStatus>UNKNOWN</upnp:writeStatus>");
+			sb.AppendLine("<dc:title>" + title + "</dc:title>");
+			sb.AppendLine("<upnp:class>object.item.videoItem</upnp:class>");
+			sb.AppendLine("<dc:date>" + date + "</dc:date>");
+			sb.AppendLine("<res protocolInfo=\"http-get:*:" + mime + ":*\" duration=\"" + time + "\" size=\"" + size + "\">" + fileUri + "</res>");
+			sb.AppendLine("</item></DIDL-Lite>");
+			return sb.ToString();
+		}
 
-			Console.WriteLine("UDP-Socket setup done...\r\n");
-
-			string SearchString = "M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nMAN:\"ssdp:discover\"\r\nST:ssdp:all\r\nMX:3\r\n\r\n";
-
-			UdpSocket.SendTo(Encoding.UTF8.GetBytes(SearchString), SocketFlags.None, MulticastEndPoint);
-
-			Console.WriteLine("M-Search sent...\r\n");
-
-			byte[] ReceiveBuffer = new byte[64000];
-
-			int ReceivedBytes = 0;
-
-			string msg = "";
-			while (true) {
-				if (UdpSocket.Available > 0) {
-					ReceivedBytes = UdpSocket.Receive(ReceiveBuffer, SocketFlags.None);
-
-					if (ReceivedBytes > 0) {
-						msg = Encoding.UTF8.GetString(ReceiveBuffer, 0, ReceivedBytes);
-						Console.WriteLine(msg);
-						break;
-					}
-				}
-			}
-            string ip="", port = "";
-			Match m = Regex.Match(msg, @"LOCATION:.*?http://(.*?):(.*?)/");
-			if (m.Success) {
-				ip   = m.Groups[1].Value;
-				port = m.Groups[2].Value;
-				deviceAddr = "http://" + ip + ":" + port;
-			}
-			Console.WriteLine("Адрес устройства воспроизведения (Renderer): " + deviceAddr);
+		private string GetMIMEfromExt(string extension) {
+			string mime;
+			return VideoMIMEmappings.TryGetValue(extension, out mime) ? mime : "video/mp4";
 		}
 
 		private static IDictionary<string, string> VideoMIMEmappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-
 			#region Big freaking list of mime types
 			{".3g2" , "video/3gpp2"},
 			{".3gp" , "video/3gpp"},
@@ -221,8 +294,78 @@ namespace testdlna {
 			{".wmx" , "video/x-ms-wmx"},
 			{".wvx" , "video/x-ms-wvx"},
 			#endregion
-
         };
 
+		/// <summary>
+		/// Запись в лог-файл сообщения
+		/// </summary>
+		/// <param name="msg">Сообщение, которое будет записано в лог-файл</param>
+		public static void LogMe(string msg) {
+			if (File.Exists(LogFile)) {
+				FileInfo fileInfo = new FileInfo(LogFile);
+				// Если он такой большой, значит забытый и не нужный - удаляем, чтобы начать всё заного
+				if (fileInfo.Length > MaxLogSize)
+					File.Delete(LogFile);
+			}
+			File.AppendAllText(LogFile, DateTime.Now.ToString() + " " + msg + "\n");
+		}
 	}
+
+	class UPnPDevice {
+		public string USN  = "";
+		public string UDN  = "";
+		public string Name = "";
+		public string Type = "";
+		public string Manufacturer = "";
+		public string ModelName    = "";
+		public string Location     = "";
+		public string Server       = "";
+		public string HeadersInfo  = "";
+		public string DeviceAddr   = "";
+		public bool   Ready        = false;
+
+		public UPnPServiceList Services = new UPnPServiceList();
+
+		public UPnPService GetService(string serviceName) {
+			foreach (UPnPService s in Services)
+				if (s.ServiceType.IndexOf(serviceName)>=0) return s;
+			return new UPnPService();
+		}
+
+		public UPnPDevice(string usn) {
+			USN = usn;
+		}
+
+		public override string ToString() { return Name + " (" + DeviceAddr + ")"; }
+	}
+
+	class UPnPService {
+        public string ServiceType  = "";
+		public string ServiceID    = "";
+		public string SCPDURL      = "";
+		public string ControlUrl   = "";
+		public string EventSubURL  = "";
+		public override string ToString() { return ServiceType + " (" + ControlUrl + ")"; }
+	}
+
+	class UPnPDeviceList : List<UPnPDevice> {
+		public bool Exists(string usn) {
+			foreach (var device in this)
+				if (device.USN == usn) return true;
+			return false;
+		}
+
+		public UPnPDevice Select(string keyword) {
+			keyword = keyword.ToLower();
+			foreach (UPnPDevice d in this) {
+				string allInfo = d.HeadersInfo + d.Manufacturer + d.Name + d.ModelName + d.UDN;
+                if (allInfo.ToLower().IndexOf(keyword) >= 0) return d;
+			}
+			return new UPnPDevice("");
+        }
+	}
+
+	class UPnPServiceList: List<UPnPService> {
+	}
+
 }
--- testdlna/Program.cs
+++ testdlna/Program.cs
@@ -1,48 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Bend.Util;
 using System.Net;
-using System.IO;
 
 namespace testdlna {
 	class Program {
+
 		static void Main(string[] args) {
-
-			string userAgent  = "SEC_HHP_Flipps/1.0";
-			string deviceIP   = "192.168.2.3";
-			string devicePort = "52235";
+    
+			bool   debug      = false;                 // флаг отладки (ведения лог-файла)
+			string userAgent  = "SEC_HHP_Flipps/1.0";  // User-Agent для запросов устройству
+			string deviceIP   = "192.168.2.3";         // IP адрес устройства, которому будет отправлен файл (вместо IP может быть любое ключевое слово, по которому можно будет идентифицировать устройство)
 			string fileURI    = "http://5.9.84.176:8080/app/2LES5e/sd0lGeslRbp8GCXTHsdpEE3jfNaogpnMB68c8bzqbIWRwR3xz8TPVxIx76W+7ijJPgNdKSL0fadYXoPyIdgXco9FWmwFnqVm703hEhet23JILKNo/A1xDVyAAgzwZnyBXqmqwRpbnG3gKK1+hU+ix5j9L5/yvOlSREqLIZ9baL3ozyliyKif0IL53g8wUKmr2RVJx+mimjtXgVttg/oa3myKT4nW4OvvL2n8vKJQxZvcQf3U57o722gGswA8XkcgEKOVjKS0zOC6eAG/+68YWeXegcgtwgeVkcNKMV6B9iO60x+A1lldRH+PwNLBCdahHBKyeJM0ZEIkLsUg3F9eGwYW9EN44GpgV6Wmb0+cn1wnljVtNhPpxDWQQMZ2+H2vpTBD/uChg/Vea8GpRkO+IA4EdLruyYgSAS6LS4q0R2ZrzKxVOszbtHucM.mp4";
+			//string fileURI    = "http://cxz.to/get/dl/6fmpm2g2rmr6147s8u1ot8txa.0.1139013157.2185543202.1447516225/American.Ultra.2015.D.BDRip.1080p.360.mp4";
+			//string fileURI = "http://n25.filecdn.to/ff/MWUyZTgzZDlkY2U0NzhjMjY1ZGM4NzQ3MGYxOGFmOGF8ZnN0b3wzOTY4MjM4NXwxMDAwMHwwfDB8bnwyNXxhZWJjNDVhZTJjMzc4ZmM0MzlmYzhmNmZkNDVkMWQxMXwxfDI2OnEuMzY6ZXwwfDgzMjUyNDU2OHwxNDQ3NTE5MDU3LjM4NDI,/American.Ultra.2015.D.BDRip.1080p.360.mp4";
+			//string fileURI = "http://pw22.poiuytrew.pw/s/44684c4f59a220101562131cd5333d75/The-Muppets-2015-USA/s01e03_480.mp4";
+			//string fileURI = "http://www.youtube.com/watch?v=25pzrnf8xyk";
+			if (args.Length > 0) deviceIP   = args[0]; // Первый параметр, елси указан - IP устройства, которому будет отправлен файл
+			if (args.Length > 1) {                     // Если указаны другие параметры, проверяем их
+				fileURI   = GetKey(args, "-(f|file)"     , fileURI  ); // Отправляемый файл на телек
+				userAgent = GetKey(args, "-(a|useragent)", userAgent); // User-Agent, который будет использован для общения с устройством
+				debug     = ChkKey(args, "-d");        // Режим отладки - запись ответов от устройства в лог-файл
+			}
 
-			if (args.Length > 0) deviceIP   = args[0];
-			if (args.Length > 1) devicePort = args[1];
-			if (args.Length > 2) userAgent  = args[2];
-			if (args.Length > 3) fileURI    = args[3];
+			fileURI = CheckKnownLinks(fileURI);
 
-			Console.WriteLine("Устройство: " + deviceIP + ":" + devicePort);
+			/*
+			HttpServer httpServer = new MyHttpServer(8368);
+			Thread thread = new Thread(new ThreadStart(httpServer.listen));
+			thread.Start();
+
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+			return;
+			*/
+			Console.WriteLine("Устройство: " + deviceIP);
 			Console.WriteLine("Файл: " + fileURI);
 			Console.WriteLine("");
 
-			DLNA dlnaDevice = new DLNA(deviceIP, devicePort);
+			DLNA dlna = new DLNA();
 
-			dlnaDevice.UserAgent = userAgent;
+			dlna.Debug     = debug;
+			dlna.UserAgent = userAgent;
 
 			Console.WriteLine("Поиск устройств воспроизведения...");
+			dlna.SearchRenderers(true);
 
-			dlnaDevice.SearchRenderers();
+			if (!dlna.SelectDevice(deviceIP)) {
+				System.Threading.Thread.Sleep(2000);
+				dlna.SearchRenderers();
+			}
+
+			if (dlna.SelectDevice(deviceIP))
+				Console.WriteLine("Выбрано устройство: {0}", dlna.SelectedDevice.Name);
+			else {
+				Console.WriteLine("Устройство {0} не найдено.", deviceIP);
+				return;
+			}
+
+			if (!dlna.SelectedDevice.Ready) {
+				Console.WriteLine("Выбранное устройство не готово или не предназначено для воспроизведения видео.");
+				return;
+			}
+
+			dlna.Stop();
 
 			Console.WriteLine("Установка адреса файла...");
-
-			dlnaDevice.SetFile(fileURI, "Advertisement", "00:00:30.000", "2015-11-04T11:54:23");
-
-			Console.WriteLine("Ответ от устройства: \n" + dlnaDevice.responseText);
-			Console.WriteLine(dlnaDevice.responseBody);
+			dlna.SetFile(fileURI, "Advertisement", "02:00:00.000", "2015-11-04T11:54:23");
 
 			Console.WriteLine("Play...");
+			dlna.Play();
 
-			dlnaDevice.Play();
+			//Console.WriteLine("Press any key...");
+			//Console.ReadKey();
+		}
 
-			Console.WriteLine("Ответ от устройства: \n" + dlnaDevice.responseText);
-			Console.WriteLine(dlnaDevice.responseBody);
+
+		/// <summary>
+		/// Получение значение указанного ключа в параметрах запуска программы.
+		/// </summary>
+		/// <param name="args">Аргументы запуска программы</param>
+		/// <param name="key">Проверяемый ключ, значение которого проверяется</param>
+		/// <param name="defaultValue">Значение по-умолчанию, которое будет возвращено, если ключ не указан в параметрах</param>
+		/// <returns>Возвращает значение указанного ключа или значение defaultValue, если указанный параметр не присутствует в параметрах запуска программы</returns>
+		static string GetKey(string[] args, string key, string defaultValue) {
+			foreach (string arg in args) {
+				Match m = Regex.Match(arg, key + "\\s*?\"(?<val>.*?)\"");
+				if (m.Success) return m.Groups["val"].Value;
+			}
+			return defaultValue;
+		}
+
+		/// <summary>
+		/// Проверка наличия ключа в параметрах запуска программы
+		/// </summary>
+		/// <param name="args">Аргументы запуска программы</param>
+		/// <param name="key">Проверяемый ключ</param>
+		/// <param name="defaultValue">Значение по-умолчанию. Необязательный параметры. По-умолчанию False.</param>
+		/// <returns>Возвращает True, если указанный параметр присутствует в параметрах запуска программы</returns>
+		static bool ChkKey(string[] args, string key, bool defaultValue=false) {
+			foreach (string arg in args) {
+				if (Regex.IsMatch(arg, key)) return true;
+			}
+			return defaultValue;
+		}
+
+		static string CheckKnownLinks(string link) {
+			Match m = Regex.Match(link, @"youtube.*?\?v=([\w_-]+)");
+			if (m.Success) {
+				string videoID = m.Groups[1].Value;
+				WebClient client = new WebClient();
+				link = client.DownloadString("https://hms.lostcut.net/youtube/g.php?v=" + videoID + "&link_only=1");
+			}
+			return link;
 		}
 	}
 }
